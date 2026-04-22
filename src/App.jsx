f8f73b639a5e4a6518c946de6dae9a2bccd7ec37
S <shwets@deloitte.com>
2026-04-22 15:24:25 +0530

    fix: add missing App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import Blog from './pages/Blog'
+import About from './pages/About'
+import PostDetail from './pages/PostDetail'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/:id" element={<PostDetail />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  )
+}
